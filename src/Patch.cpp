5f2ab4df7783fedc6f24d7f76be357f40c01a85a
praydog <praydog@gmail.com>
2019-02-01 10:14:45 -0800

    Style cleanup

--- src/Patch.cpp
+++ src/Patch.cpp
@@ -16,6 +16,39 @@ std::unique_ptr<Patch> Patch::createNOP(uintptr_t addr, uint32_t length) {
     return std::make_unique<Patch>(addr, bytes);
 }
 
+bool Patch::patch(uintptr_t address, const vector<int16_t>& bytes) {
+    auto oldProtection = protect(address, bytes.size(), PAGE_EXECUTE_READWRITE);
+
+    if (!oldProtection) {
+        return false;
+    }
+
+    unsigned int count = 0;
+
+    for (auto byte : bytes) {
+        if (byte >= 0 && byte <= 0xFF) {
+            *(uint8_t*)(address + count) = (uint8_t)byte;
+        }
+
+        ++count;
+    }
+
+    FlushInstructionCache(GetCurrentProcess(), (LPCVOID)address, bytes.size());
+    protect(address, bytes.size(), *oldProtection);
+
+    return true;
+}
+
+optional<DWORD> Patch::protect(uintptr_t address, size_t size, DWORD protection) {
+    DWORD oldProtection{ 0 };
+
+    if (VirtualProtect((LPVOID)address, size, protection, &oldProtection) != FALSE) {
+        return oldProtection;
+    }
+
+    return {};
+}
+
 Patch::Patch(uintptr_t addr, const std::vector<int16_t>& b, bool shouldEnable /*= true*/) 
     : m_address{ addr },
     m_bytes{ b }
@@ -59,37 +92,4 @@ bool Patch::toggle() {
 
 bool Patch::toggle(bool state) {
     return state ? enable() : disable();
-}
-
-bool Patch::patch(uintptr_t address, const vector<int16_t>& bytes) {
-    auto oldProtection = protect(address, bytes.size(), PAGE_EXECUTE_READWRITE);
-
-    if (!oldProtection) {
-        return false;
-    }
-
-    unsigned int count = 0;
-
-    for (auto byte : bytes) {
-        if (byte >= 0 && byte <= 0xFF) {
-            *(uint8_t*)(address + count) = (uint8_t)byte;
-        }
-
-        ++count;
-    }
-
-    FlushInstructionCache(GetCurrentProcess(), (LPCVOID)address, bytes.size());
-    protect(address, bytes.size(), *oldProtection);
-
-    return true;
-}
-
-optional<DWORD> Patch::protect(uintptr_t address, size_t size, DWORD protection) {
-    DWORD oldProtection{ 0 };
-
-    if (VirtualProtect((LPVOID)address, size, protection, &oldProtection) != FALSE) {
-        return oldProtection;
-    }
-
-    return {};
 }
--- src/Patch.hpp
+++ src/Patch.hpp
@@ -13,6 +13,11 @@ public:
     static Ptr create(uintptr_t addr, const std::vector<int16_t>& b);
     static Ptr createNOP(uintptr_t addr, uint32_t length);
 
+    static bool patch(uintptr_t address, const std::vector<int16_t>& bytes);
+    // Returns the old protection on success.
+    // Note: you don't need to call this if you're using the above patch function.
+    static std::optional<DWORD> protect(uintptr_t address, size_t size, DWORD protection);
+
     Patch(uintptr_t addr, const std::vector<int16_t>& b, bool shouldEnable = true);
 
     virtual ~Patch();
@@ -21,11 +26,6 @@ public:
     bool disable();
     bool toggle();
     bool toggle(bool state);
-
-    static bool patch(uintptr_t address, const std::vector<int16_t>& bytes);
-    // Returns the old protection on success.
-    // Note: you don't need to call this if you're using the above patch function.
-    static std::optional<DWORD> protect(uintptr_t address, size_t size, DWORD protection);
 
 private:
     uintptr_t m_address{ 0 };
