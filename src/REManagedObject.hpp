4d2b73f84d168452169b7f25c1b10a13e0221abe
praydog <praydog@gmail.com>
2019-02-06 20:35:23 -0800

    Expand on utility::REManagedObject

--- src/REManagedObject.hpp
+++ src/REManagedObject.hpp
@@ -1,26 +1,59 @@
 #pragma once
 
+#include "Address.hpp"
 #include "ReClass.hpp"
 
 namespace utility::REManagedObject {
-    static bool isA(::REManagedObject* object, std::string_view name) {
-        if (object == nullptr) {
+    static bool isManagedObject(Address address) {
+        if (address == nullptr) {
             return false;
+        }
+
+        if (IsBadReadPtr(address.ptr(), sizeof(void*))) {
+            return false;
+        }
+
+        auto object = address.as<::REManagedObject*>();
+
+        if (object->info == nullptr || IsBadReadPtr(object->info, sizeof(void*))) {
+            return false;
+        }
+
+        auto classInfo = object->info->classInfo;
+
+        if (classInfo == nullptr || IsBadReadPtr(classInfo, sizeof(void*))) {
+            return false;
+        }
+
+        return classInfo->parentInfo == object->info && classInfo->type != nullptr && classInfo->type->name != nullptr;
+    }
+
+    static REType* getType(::REManagedObject* object) {
+        if (object == nullptr) {
+            return nullptr;
         }
 
         auto info = object->info;
 
         if (info == nullptr) {
-            return false;
+            return nullptr;
         }
 
         auto classInfo = info->classInfo;
 
         if (classInfo == nullptr) {
+            return nullptr;
+        }
+
+        return classInfo->type;
+    }
+
+    static bool isA(::REManagedObject* object, std::string_view name) {
+        if (object == nullptr) {
             return false;
         }
 
-        for (auto t = classInfo->type; t != nullptr && t->name != nullptr; t = t->super) {
+        for (auto t = REManagedObject::getType(object); t != nullptr && t->name != nullptr; t = t->super) {
             if (name == t->name) {
                 return true;
             }
