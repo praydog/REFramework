6e42f46c3c43870b394c62199f7e69681195ad70
praydog <praydog@praydog.com>
2021-07-11 07:42:28 -0700

    Fix build_vs2019.bat

--- build_vs2019.bat
+++ build_vs2019.bat
@@ -2,5 +2,5 @@ git pull --recurse-submodules
 git submodule update --init --recursive
 mkdir build64_all
 cd build64_all
-cmake .. -G "Visual Studio 16 2019" -A x64 -DDEVELOPER_MODE=OFF -DRE2=ON
+cmake .. -G "Visual Studio 16 2019" -A x64 -DDEVELOPER_MODE=OFF
 cmake --build . --config Release
