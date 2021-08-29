3c91200413680c4c9aeba748eeac7f7a7a6adca9
praydog <praydog@praydog.com>
2021-08-29 12:33:19 -0700

    More RETransform utilities

--- src/sdk/RETransform.hpp
+++ src/sdk/RETransform.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
+#include <vector>
+
 #include "Math.hpp"
 
 namespace utility::re_transform {
+    // because the ReClass version looks like ass
+    struct Joint : public REManagedObject {
+		::RETransform* Owner;
+		::REJointDesc* Desc;
+		Vector4f LocalPosition;
+		Vector4f LocalRotation;
+		Vector4f LocalScale;
+		int32_t ConstraintJointIndex;
+		int32_t JointIndex;
+	};
+
     static Matrix4x4f invalid_matrix{};
 
     static REJoint* get_joint(const ::RETransform& transform, uint32_t index) {
@@ -60,10 +73,88 @@ namespace utility::re_transform {
     static Matrix4x4f& get_joint_matrix(const ::RETransform& transform, std::wstring_view name) {
         auto joint = get_joint(transform, name);
 
-        if (joint != nullptr) {
-            return transform.joints.matrices->data[joint->info->jointNumber].worldMatrix;
+        if (joint != nullptr && joint->info != nullptr) {
+            return transform.joints.matrices->data[((Joint*)joint)->JointIndex].worldMatrix;
         }
 
         return invalid_matrix;
+    }
+
+    static Matrix4x4f& get_joint_matrix(const ::RETransform& transform, REJoint* joint) {
+        if (joint != nullptr && joint->info != nullptr) {
+            return transform.joints.matrices->data[((Joint*)joint)->JointIndex].worldMatrix;
+        }
+
+        return invalid_matrix;
+    }
+
+    static std::vector<REJoint*> get_all_children(const ::RETransform& transform, REJoint* parent, std::unordered_set<REJoint*>& visited) {
+        if (parent->info == nullptr || visited.contains(parent)) {
+            return {};
+        }
+        
+        visited.insert(parent);
+
+        std::vector<REJoint*> children{};
+
+        for (int32_t i = 0; i < transform.joints.size; ++i) {
+            auto joint = transform.joints.data->joints[i];
+
+            if (joint == nullptr || joint == parent) {
+                continue;
+            }
+
+            auto joint_info = joint->info;
+
+            if (joint_info == nullptr || joint_info->name == nullptr) {
+                continue;
+            }
+
+            if (joint->info->parentJoint == ((Joint*)parent)->JointIndex) {
+                children.push_back(joint);
+
+                auto child_children = get_all_children(transform, joint, visited);
+
+                for (auto child_child : child_children) {
+                    children.push_back(child_child);
+                }
+            }
+        }
+
+        return children;
+    }
+
+    static std::vector<REJoint*> get_all_children(const ::RETransform& transform, REJoint* parent) {
+        std::unordered_set<REJoint*> visited{};
+
+        return get_all_children(transform, parent, visited);
+    }
+
+    static std::vector<REJoint*> get_immediate_children(const ::RETransform& transform, REJoint* parent) {
+        if (parent->info == nullptr) {
+            return {};
+        }
+
+        std::vector<REJoint*> children{};
+
+        for (int32_t i = 0; i < transform.joints.size; ++i) {
+            auto joint = transform.joints.data->joints[i];
+
+            if (joint == nullptr || joint == parent) {
+                continue;
+            }
+
+            auto joint_info = joint->info;
+
+            if (joint_info == nullptr || joint_info->name == nullptr) {
+                continue;
+            }
+
+            if (joint->info->parentJoint == ((Joint*)parent)->JointIndex) {
+                children.push_back(joint);
+            }
+        }
+
+        return children;
     }
 }
