7a80950f86f41ca1cd25e457fb588a62ec299222
praydog <praydog@praydog.com>
2022-01-11 17:17:45 -0800

    Possible fix for a rare startup crash

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,40 @@
 #include <thread>
 #include <chrono>
+#include <mutex>
 #include <windows.h>
 
 #include "ExceptionHandler.hpp"
 #include "REFramework.hpp"
 
 HMODULE g_dinput = 0;
+std::mutex g_load_mutex{};
+
+void failed() {
+    MessageBox(0, "REFramework: Unable to load the original dinput8.dll. Please report this to the developer.", "REFramework", 0);
+    ExitProcess(0);
+}
+
+bool load_dinput8() {
+    std::scoped_lock _{g_load_mutex};
+
+    if (g_dinput) {
+        return true;
+    }
+
+    wchar_t buffer[MAX_PATH]{ 0 };
+    if (GetSystemDirectoryW(buffer, MAX_PATH) != 0) {
+        // Load the original dinput8.dll
+        if ((g_dinput = LoadLibraryW((std::wstring{ buffer } + L"\\dinput8.dll").c_str())) == NULL) {
+            failed();
+            return false;
+        }
+
+        return true;
+    }
+
+    failed();
+    return false;
+}
 
 extern "C" {
     // DirectInput8Create wrapper for dinput8.dll
@@ -13,13 +42,10 @@ extern "C" {
         // This needs to be done because when we include dinput.h in DInputHook,
         // It is a redefinition, so we assign an export by not using the original name
         #pragma comment(linker, "/EXPORT:DirectInput8Create=direct_input8_create")
+
+        load_dinput8();
         return ((decltype(DirectInput8Create)*)GetProcAddress(g_dinput, "DirectInput8Create"))(hinst, dw_version, riidltf, ppv_out, punk_outer);
     }
-}
-
-void failed() {
-    MessageBox(0, "REFramework: Unable to load the original dinput8.dll. Please report this to the developer.", "REFramework", 0);
-    ExitProcess(0);
 }
 
 void startup_thread() {
@@ -34,17 +60,8 @@ void startup_thread() {
     freopen("CONOUT$", "w", stderr);
 #endif
 
-    wchar_t buffer[MAX_PATH]{ 0 };
-    if (GetSystemDirectoryW(buffer, MAX_PATH) != 0) {
-        // Load the original dinput8.dll
-        if ((g_dinput = LoadLibraryW((std::wstring{ buffer } + L"\\dinput8.dll").c_str())) == NULL) {
-            failed();
-        }
-
+    if (load_dinput8()) {
         g_framework = std::make_unique<REFramework>();
-    }
-    else {
-        failed();
     }
 }
 
