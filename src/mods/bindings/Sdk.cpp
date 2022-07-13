07ab146fadecc801b3935c93dac8172fbd220e0a
praydog <praydog@praydog.com>
2022-07-12 20:33:31 -0700

    Lua: Fix new object indexers not working on derived classes

--- src/mods/bindings/Sdk.cpp
+++ src/mods/bindings/Sdk.cpp
@@ -1052,6 +1052,31 @@ void hook(sol::this_state s, ::sdk::REMethodDefinition* fn, sol::protected_funct
 }
 
 namespace api::re_managed_object {
+sol::object index(sol::this_state s, REManagedObject* obj, const char* name) {
+    if (obj == nullptr) {
+        return sol::make_object(s, sol::nil);
+    }
+
+    auto type_def = utility::re_managed_object::get_type_definition(obj);
+    auto field = type_def->get_field("name");
+    if (field != nullptr) {
+        return api::sdk::get_native_field_from_field(sol::make_object(s, obj), type_def, field);
+    }
+    auto method = type_def->get_method(name);
+    if (method != nullptr) {
+        return sol::make_object(s, method);
+    }
+    return sol::make_object(s, sol::nil);
+}
+
+void new_index(sol::this_state s, REManagedObject* obj, const char* name, sol::object value) {
+    if (obj == nullptr) {
+        return;
+    }
+
+    return api::sdk::set_native_field(sol::make_object(s, obj), utility::re_managed_object::get_type_definition(obj), name, value); 
+}
+
 bool is_valid_offset(::REManagedObject* obj, int32_t offset) {
     if (obj == nullptr || !::utility::re_managed_object::is_managed_object(obj)) {
         return false;
@@ -1302,29 +1327,8 @@ void bindings::open_sdk(ScriptState* s) {
 
     lua.new_usertype<::REManagedObject>("REManagedObject",
         sol::meta_function::equal_to, [s](REManagedObject* lhs, REManagedObject* rhs) { return lhs == rhs; },
-        sol::meta_function::index,  [s](REManagedObject* obj, const char* name) {
-            if (obj == nullptr) {
-                return sol::make_object(s->lua(), sol::nil);
-            }
-
-            auto type_def = utility::re_managed_object::get_type_definition(obj);
-            auto field = type_def->get_field("name");
-            if (field != nullptr) {
-                return api::sdk::get_native_field_from_field(sol::make_object(s->lua(), obj), type_def, field);
-            }
-            auto method = type_def->get_method(name);
-            if (method != nullptr) {
-                return sol::make_object(s->lua(), method);
-            }
-            return sol::make_object(s->lua(), sol::nil);
-        },
-        sol::meta_function::new_index, [s](REManagedObject* obj, const char* name, sol::object value){
-            if (obj == nullptr) {
-                return;
-            }
-
-            return api::sdk::set_native_field(sol::make_object(s->lua(), obj), utility::re_managed_object::get_type_definition(obj), name, value); 
-        },
+        sol::meta_function::index, &api::re_managed_object::index,
+        sol::meta_function::new_index, &api::re_managed_object::new_index,
         "add_ref", &api::re_managed_object::add_ref,
         "add_ref_permanent", &api::re_managed_object::add_ref_permanent,
         "force_release", [](sol::this_state s, ::REManagedObject* obj) {
@@ -1396,12 +1400,16 @@ void bindings::open_sdk(ScriptState* s) {
     create_managed_object_ptr_gc((::REManagedObject*)nullptr);
 
     lua.new_usertype<REComponent>("REComponent",
-        sol::base_classes, sol::bases<::REManagedObject>()
+        sol::base_classes, sol::bases<::REManagedObject>(),
+        sol::meta_function::index, &api::re_managed_object::index,
+        sol::meta_function::new_index, &api::re_managed_object::new_index
     );
 
     create_managed_object_ptr_gc((::REComponent*)nullptr);
 
     lua.new_usertype<RETransform>("RETransform",
+        sol::meta_function::index, &api::re_managed_object::index,
+        sol::meta_function::new_index, &api::re_managed_object::new_index,
         "calculate_base_transform", &utility::re_transform::calculate_base_transform,
         "calculate_tpose_pos_world", &utility::re_transform::calculate_tpose_pos_world,
         "apply_joints_tpose", [](RETransform* t, sol::object joints, uint32_t additional_parents) {
@@ -1550,6 +1558,8 @@ void bindings::open_sdk(ScriptState* s) {
     );
 
     lua.new_usertype<api::sdk::BehaviorTreeCoreHandle>("BehaviorTreeCoreHandle",
+        sol::meta_function::index, &api::re_managed_object::index,
+        sol::meta_function::new_index, &api::re_managed_object::new_index,
         sol::base_classes, sol::bases<::REManagedObject>(),
         "get_tree_object", [](api::sdk::BehaviorTreeCoreHandle* handle) {
             return ((sdk::behaviortree::CoreHandle*)handle)->get_tree_object();
@@ -1559,6 +1569,8 @@ void bindings::open_sdk(ScriptState* s) {
     create_managed_object_ptr_gc((api::sdk::BehaviorTreeCoreHandle*)nullptr);
 
     lua.new_usertype<api::sdk::BehaviorTree>("BehaviorTree",
+        sol::meta_function::index, &api::re_managed_object::index,
+        sol::meta_function::new_index, &api::re_managed_object::new_index,
         sol::base_classes, sol::bases<::REManagedObject>(),
         "get_tree", [](api::sdk::BehaviorTree* tree, uint32_t index) {
             return ((sdk::behaviortree::BehaviorTree*)tree)->get_tree<api::sdk::BehaviorTreeCoreHandle>(index);
