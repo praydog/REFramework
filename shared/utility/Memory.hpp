09e5d4db2af5fa0ece1c69a0d617b48fb876830d
praydog <praydog@praydog.com>
2022-07-22 09:00:37 -0700

    SDK/Utility: Add reusable pointer relocator

--- shared/utility/Memory.hpp
+++ shared/utility/Memory.hpp
@@ -9,4 +9,6 @@ namespace utility {
     bool isGoodCodePtr(uintptr_t ptr, size_t len);
 
     bool is_stub_code(uint8_t* code);
+
+    void relocate_pointers(uint8_t* scan_start, uintptr_t old_start, uintptr_t old_end, uintptr_t new_start, int32_t depth = 0, uint32_t skip_length = sizeof(void*), uint32_t scan_size = 0x1000);
 }
