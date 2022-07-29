48e57f52036e753f90a024d0da90cfb14aa418a4
praydog <praydog@praydog.com>
2022-07-28 19:50:11 -0700

    Fix incorrect usage of spdlog in Thread

--- shared/utility/Thread.cpp
+++ shared/utility/Thread.cpp
@@ -38,7 +38,7 @@ ThreadStates suspend_threads() {
             if (thread_handle != nullptr && snapshot_handle != INVALID_HANDLE_VALUE) {
                 auto state = std::make_unique<ThreadState>();
 
-                spdlog::info("Suspending {:i}", te.th32ThreadID);
+                spdlog::info("Suspending {}", (uint32_t)te.th32ThreadID);
 
                 state->thread_id = te.th32ThreadID;
                 state->suspended = SuspendThread(thread_handle) > 0;
