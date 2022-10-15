b35cdd2dc4e16eda107020d8f6a91e27fd4d9c1e
praydog <praydog@praydog.com>
2022-10-15 00:47:04 -0700

    VR (RE8): Fix incorrect resolution getting applied

--- shared/sdk/regenny/re8/via/Window.hpp
+++ shared/sdk/regenny/re8/via/Window.hpp
@@ -1,18 +1,18 @@
 #pragma once
-#include ".\Point.hpp"
-#include ".\Size.hpp"
+#include "Point.hpp"
+#include "Size.hpp"
 namespace regenny::via {
 #pragma pack(push, 1)
 struct Window {
     void* vtable; // 0x0
-    char pad_8[0x50];
-    uint32_t width; // 0x58
-    uint32_t height; // 0x5c
-    regenny::via::Point cursor_pos; // 0x60
-    bool show_cursor; // 0x68
-    char pad_69[0x2f];
-    regenny::via::Size borderless_size; // 0x98
-    char pad_a0[0x60];
+    char pad_8[0x68];
+    uint32_t width; // 0x70
+    uint32_t height; // 0x74
+    regenny::via::Point cursor_pos; // 0x78
+    bool show_cursor; // 0x80
+    char pad_81[0x2f];
+    regenny::via::Size borderless_size; // 0xb0
+    char pad_b8[0x48];
 }; // Size: 0x100
 #pragma pack(pop)
 }
