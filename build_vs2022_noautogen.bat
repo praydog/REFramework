72b2bdcaf790349c0bd204face32282797e75ab0
praydog <praydog@praydog.com>
2023-05-25 00:20:17 -0700

    Update build batch files

--- build_vs2022_noautogen.bat
+++ build_vs2022_noautogen.bat
@@ -2,6 +2,5 @@ git pull --recurse-submodules
 git submodule update --init --recursive
 mkdir build64_all
 cd build64_all
-cmake .. -G "Visual Studio 16 2019" -A x64 -DDEVELOPER_MODE=OFF
-::cmake .. -G "Visual Studio 17 2022" -A x64 -DDEVELOPER_MODE=OFF
+cmake .. -G "Visual Studio 17 2022" -A x64 -DDEVELOPER_MODE=ON -DCMKR_SKIP_GENERATION=ON
 cmake --build . --config Release
