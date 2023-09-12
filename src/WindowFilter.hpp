236a9b8a7a68b3a42692ce508a9f656425b640c2
praydog <praydog@praydog.com>
2023-09-11 23:06:41 -0700

    Fix case where WindowFilter could deadlock the present thread

--- src/WindowFilter.hpp
+++ src/WindowFilter.hpp
@@ -1,65 +1,35 @@
 #pragma once
 
-#include <mutex>
 #include <Windows.h>
 
 #include <string_view>
 #include <unordered_set>
+#include <thread>
+#include <vector>
+#include <mutex>
 
 class WindowFilter {
 public:
-    static WindowFilter& get() {
-        static WindowFilter instance{};
-        return instance;
-    }
+    static WindowFilter& get();
 
 public:
-    bool is_filtered(HWND hwnd) {
-        if (hwnd == nullptr) {
-            return true;
-        }
+    WindowFilter();
+    virtual ~WindowFilter();
 
-        std::scoped_lock _{m_mutex};
+    bool is_filtered(HWND hwnd);
 
-        if (m_filtered_windows.find(hwnd) != m_filtered_windows.end()) {
-            return true;
-        }
-
-        if (m_seen_windows.find(hwnd) != m_seen_windows.end()) {
-            return false;
-        }
-
-        m_seen_windows.insert(hwnd);
-
-        if (is_filtered_nocache(hwnd)) {
-            filter_window(hwnd);
-            return true;
-        }
-
-        return false;
-    }
-
-private:
     void filter_window(HWND hwnd) {
+        std::scoped_lock _{m_mutex};
         m_filtered_windows.insert(hwnd);
     }
 
-    bool is_filtered_nocache(HWND hwnd) {
-        // get window name
-        char window_name[256]{};
-        GetWindowTextA(hwnd, window_name, sizeof(window_name));
+private:
+    bool is_filtered_nocache(HWND hwnd);
 
-        const auto sv = std::string_view{window_name};
+    std::recursive_mutex m_mutex{};
+    std::vector<HWND> m_window_jobs{};
+    std::unique_ptr<std::jthread> m_job_thread{};
 
-        if (sv.find("PimaxXR") != std::string_view::npos) {
-            return true;
-        }
-
-        // TODO: more problematic windows
-        return false;
-    }
-
-    std::mutex m_mutex{};
     std::unordered_set<HWND> m_seen_windows{};
     std::unordered_set<HWND> m_filtered_windows{};
 };
