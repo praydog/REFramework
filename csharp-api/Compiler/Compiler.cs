3681e43c0deb21f082b3859e91d767f76466abc2
praydog <praydog@praydog.com>
2024-03-18 15:46:21 -0700

    .NET: Rename some stuff for more idiomatic names

--- csharp-api/Compiler/Compiler.cs
+++ csharp-api/Compiler/Compiler.cs
@@ -7,7 +7,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Text;
 
-namespace DynamicRun.Builder
+namespace REFrameworkNET
 {
     public class Compiler
     {
