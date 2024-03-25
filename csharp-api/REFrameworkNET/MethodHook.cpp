565c6e0bf0f4359d5605d1a1762f629714495880
praydog <praydog@praydog.com>
2024-03-25 03:56:29 -0700

    .NET: MethodHookWrapper->MethodHook

--- csharp-api/REFrameworkNET/MethodHook.cpp
+++ csharp-api/REFrameworkNET/MethodHook.cpp
@@ -1,11 +1,11 @@
 #include "./API.hpp"
-#include "MethodHookWrapper.hpp"
+#include "MethodHook.hpp"
 
 using namespace System::Runtime::InteropServices;
 using namespace System::Collections::Generic;
 
 namespace REFrameworkNET {
-    void MethodHookWrapper::InstallHooks(bool ignore_jmp) 
+    void MethodHook::InstallHooks(bool ignore_jmp) 
     {
         if (m_hooks_installed) {
             return;
@@ -22,7 +22,7 @@ namespace REFrameworkNET {
         m_hook_id = raw->add_hook((REFPreHookFn)preHookPtr.ToPointer(), (REFPostHookFn)postHookPtr.ToPointer(), ignore_jmp);
     }
 
-    void MethodHookWrapper::UninstallHooks() {
+    void MethodHook::UninstallHooks() {
         if (!m_hooks_installed) {
             return;
         }
@@ -33,14 +33,14 @@ namespace REFrameworkNET {
         raw->remove_hook(m_hook_id);
     }
 
-    int32_t MethodHookWrapper::OnPreStart_Raw(int argc, void** argv, REFrameworkTypeDefinitionHandle* arg_tys, unsigned long long ret_addr) 
+    int32_t MethodHook::OnPreStart_Raw(int argc, void** argv, REFrameworkTypeDefinitionHandle* arg_tys, unsigned long long ret_addr) 
     {
         OnPreStart(gcnew List<System::Object^>()); // todo: pass the arguments
         System::Console::WriteLine("Hello from" + m_method->GetName() + " pre-hook!");
         return 0; // Or another appropriate value
     }
 
-    void MethodHookWrapper::OnPostStart_Raw(void** ret_val, REFrameworkTypeDefinitionHandle ret_ty, unsigned long long ret_addr) {
+    void MethodHook::OnPostStart_Raw(void** ret_val, REFrameworkTypeDefinitionHandle ret_ty, unsigned long long ret_addr) {
         //OnPostStart(/* arguments */);
         System::Console::WriteLine("Hello from" + m_method->GetName() + " post-hook!");
     }
