01d3048267d7312ca17be697889e544797603450
praydog <praydog@praydog.com>
2024-04-04 12:36:46 -0700

    FunctionHook: Lock accessors in shared mode to fix race condition crash

--- shared/utility/FunctionHook.hpp
+++ shared/utility/FunctionHook.hpp
@@ -2,6 +2,7 @@
 
 #include <windows.h>
 #include <cstdint>
+#include <shared_mutex>
 
 #include <utility/Address.hpp>
 
@@ -18,15 +19,18 @@ public:
     bool create();
 
     auto get_original() const {
+        std::shared_lock _{ m_initialization_mutex };
         return m_inline_hook->trampoline().address();
     }
 
     template <typename T>
     T* get_original() const {
+        std::shared_lock _{ m_initialization_mutex };
         return m_inline_hook->original<T*>();
     }
 
     auto is_valid() const {
+        std::shared_lock _{ m_initialization_mutex };
         return m_inline_hook && m_inline_hook->operator bool();
     }
 
@@ -35,6 +39,7 @@ public:
 
 private:
     std::expected<SafetyHookInline, SafetyHookInline::Error> m_inline_hook;
+    std::shared_mutex m_initialization_mutex{};
 
     uintptr_t m_target{ 0 };
     uintptr_t m_destination{ 0 };
