6f15f7b1d65c05a9b76bd61cfb113d2f77274ca3
praydog <praydog@praydog.com>
2024-04-05 01:42:53 -0700

    SDK: Perf/memory improvements to field/method lookups

--- shared/sdk/RETypeDefinition.cpp
+++ shared/sdk/RETypeDefinition.cpp
@@ -419,49 +419,50 @@ sdk::RETypeDefinition* RETypeDefinition::get_generic_type_definition() const {
 }
 
 static std::shared_mutex g_field_mtx{};
-static std::unordered_map<std::string, sdk::REField*> g_field_map{};
+static std::unordered_map<const sdk::RETypeDefinition*, std::unordered_map<size_t, sdk::REField*>> g_field_map{};
 
 sdk::REField* RETypeDefinition::get_field(std::string_view name) const {
-    auto full_name = std::to_string(this->get_index()) + "." + name.data();
-
+    const auto name_hash = std::hash<std::string_view>{}(name);
     {
         std::shared_lock _{ g_field_mtx };
 
-        if (auto it = g_field_map.find(full_name); it != g_field_map.end()) {
-            return it->second;
+        if (auto it = g_field_map.find(this); it != g_field_map.end()) {
+            if (auto it2 = it->second.find(name_hash); it2 != it->second.end()) {
+                return it2->second;
+            }
         }
     }
+
 
     for (auto super = this; super != nullptr; super = super->get_parent_type()) {
         for (auto f : super->get_fields()) {
             if (name == f->get_name()) {
                 std::unique_lock _{ g_field_mtx };
 
-                g_field_map[full_name] = f;
-                return g_field_map[full_name];
+                g_field_map[this][name_hash] = f;
+                return g_field_map[this][name_hash];
             }
         }
     }
 
     std::unique_lock _{ g_field_mtx };
-    return g_field_map[full_name];
+    g_field_map[this][name_hash] = nullptr;
+    return nullptr;
 }
 
 static std::shared_mutex g_method_mtx{};
-static std::unordered_map<std::string, REMethodDefinition*> g_method_map{};
+static std::unordered_map<const sdk::RETypeDefinition*, std::unordered_map<size_t, sdk::REMethodDefinition*>> g_method_map{};
 
 sdk::REMethodDefinition* RETypeDefinition::get_method(std::string_view name) const {
-    // originally this used this->get_full_name() + "." + name.data()
-    // but that doesn't work for generic types if we haven't yet mapped out
-    // how generic (instantiated) types work for the game we're working with
-    // and this is probably faster anyways
-    auto full_name = std::to_string(this->get_index()) + "." + name.data();
+    const auto name_hash = std::hash<std::string_view>{}(name);
 
     {
         std::shared_lock _{g_method_mtx};
 
-        if (auto it = g_method_map.find(full_name); it != g_method_map.end()) {
-            return it->second;
+        if (auto it = g_method_map.find(this); it != g_method_map.end()) {
+            if (auto it2 = it->second.find(name_hash); it2 != it->second.end()) {
+                return it2->second;
+            }
         }
     }
     
@@ -471,8 +472,8 @@ sdk::REMethodDefinition* RETypeDefinition::get_method(std::string_view name) con
             if (name == m.get_name()) {
                 std::unique_lock _{g_method_mtx};
 
-                g_method_map[full_name] = &m;
-                return g_method_map[full_name];
+                g_method_map[this][name_hash] = &m;
+                return g_method_map[this][name_hash];
             }
         }
     }
@@ -499,14 +500,14 @@ sdk::REMethodDefinition* RETypeDefinition::get_method(std::string_view name) con
             if (name == method_prototype) {
                 std::unique_lock _{g_method_mtx};
 
-                g_method_map[full_name] = &m;
-                return g_method_map[full_name];
+                g_method_map[this][name_hash] = &m;
+                return g_method_map[this][name_hash];
             }
         }
     }
 
     std::unique_lock _{g_method_mtx};
-    return g_method_map[full_name];
+    return g_method_map[this][name_hash] = nullptr;
 }
 
 std::vector<sdk::REMethodDefinition*> RETypeDefinition::get_methods(std::string_view name) const {
