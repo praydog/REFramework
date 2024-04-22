4e55776818dba4a284a90ebbdc86c39ab05f7f31
praydog <praydog@praydog.com>
2024-04-22 03:14:18 -0700

    FunctionHook: Reduce lock contention

--- shared/utility/FunctionHook.hpp
+++ shared/utility/FunctionHook.hpp
@@ -19,18 +19,33 @@ public:
     bool create();
 
     auto get_original() const {
+        if (m_init_finished) {
+            return m_inline_hook->trampoline().address();
+        }
+
         std::shared_lock _{ m_initialization_mutex };
+        m_init_finished = true;
         return m_inline_hook->trampoline().address();
     }
 
     template <typename T>
     T* get_original() const {
+        if (m_init_finished) {
+            return m_inline_hook->original<T*>();
+        }
+
         std::shared_lock _{ m_initialization_mutex };
+        m_init_finished = true;
         return m_inline_hook->original<T*>();
     }
 
     auto is_valid() const {
+        if (m_init_finished) {
+            return is_valid_unsafe();
+        }
+
         std::shared_lock _{ m_initialization_mutex };
+        m_init_finished = true;
         return is_valid_unsafe();
     }
 
@@ -44,6 +59,7 @@ private:
 
     std::expected<SafetyHookInline, SafetyHookInline::Error> m_inline_hook;
     mutable std::shared_mutex m_initialization_mutex{};
+    mutable bool m_init_finished{ false };
 
     uintptr_t m_target{ 0 };
     uintptr_t m_destination{ 0 };
