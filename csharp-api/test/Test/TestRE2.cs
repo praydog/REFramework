cda00ff364a6c2ccae882f483232b836d45cdfc9
praydog <praydog@praydog.com>
2024-04-22 08:58:55 -0700

    .NET: More detailed bench stats for RE2

--- csharp-api/test/Test/TestRE2.cs
+++ csharp-api/test/Test/TestRE2.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Reflection;
+using System.Threading;
+using app.ropeway;
 using ImGuiNET;
 using REFrameworkNET;
 using REFrameworkNET.Callbacks;
@@ -26,11 +28,36 @@ public class TestRE2Plugin {
                 ImGui.Text("RE2");
                 ImGui.Separator();
 
-                ImGui.Text("Benchmark average: " + RE2HookBenchmark.RunningAvg.ToString("0.000") + " µs");
-                ImGui.Text("Benchmark count: " + RE2HookBenchmark.MeasureCount);
+                System.Collections.Generic.List<long> threadRanks = new();
 
-                ImGui.PlotLines("Benchmark", ref RE2HookBenchmark.BenchmarkData[0], 1000, 0, "µs", 0, (float)RE2HookBenchmark.HighestMicros, new System.Numerics.Vector2(0, 80));
+                foreach(var tdata in RE2HookBenchmark.threadData) {
+                    threadRanks.Add(tdata.Value.threadID);
+                }
 
+                // Sort by highest running average
+                threadRanks.Sort((a, b) => {
+                    var aData = RE2HookBenchmark.threadData[a];
+                    var bData = RE2HookBenchmark.threadData[b];
+                    return aData.runningAvg.CompareTo(bData.runningAvg);
+                });
+
+                var totalThreadRanks = threadRanks.Count;
+
+                foreach(var tdata in RE2HookBenchmark.threadData) {
+                    var rank = threadRanks.IndexOf(tdata.Value.threadID) + 1;
+                    var greenColor = 1.0f - (float)rank / (float)totalThreadRanks;
+                    var redColor = (float)rank / (float)totalThreadRanks;
+
+                    //ImGui.Text("Thread ID: " + tdata.Value.threadID + " Avg: " + tdata.Value.runningAvg.ToString("0.000") + " µs");
+                    ImGui.PushStyleColor(ImGuiCol.Text, new System.Numerics.Vector4(redColor, greenColor, 0f, 1.0f));
+                    ImGui.PlotLines("Thread " + tdata.Value.threadID, ref tdata.Value.benchmarkData[0], 1000, tdata.Value.callCount % 1000, tdata.Value.runningAvg.ToString("0.000") + " µs", 0, (float)tdata.Value.runningAvg * 2.0f, new System.Numerics.Vector2(0, 30));
+                    ImGui.PopStyleColor();
+                }
+
+                ImGui.PushStyleColor(ImGuiCol.Text, new System.Numerics.Vector4(0.5f, 1f, 0.4f, 1.0f));
+                ImGui.PlotLines("Overall Benchmark", ref RE2HookBenchmark.BenchmarkData[0], 1000, RE2HookBenchmark.MeasureCount % 1000, RE2HookBenchmark.RunningAvg.ToString("0.000") + " µs", 0, (float)RE2HookBenchmark.RunningAvg * 2.0f, new System.Numerics.Vector2(0, 40));
+                ImGui.PopStyleColor();
+                
                 if (ImGui.TreeNode("Player")) {
                     var playerManager = API.GetManagedSingletonT<app.ropeway.PlayerManager>();
                     var player = playerManager.get_CurrentPlayer();
@@ -47,13 +74,15 @@ public class TestRE2Plugin {
         };
 
         // Benchmarking the effects of threading on invoking game code
-        for (int i = 0; i < 8; ++i) {
+        for (int i = 0; i < 2; ++i) {
             threads.Add(new System.Threading.Thread(() => {
-                while (!cts.Token.IsCancellationRequested) {
+                /*while (!cts.Token.IsCancellationRequested) {
                     RE2HookBenchmark.Bench(BenchFnAction);
                     // We must manually call the GC in our own threads not owned by the game
                     API.LocalFrameGC();
-                }
+                }*/
+
+                API.LocalFrameGC();
             }));
         }
 
@@ -70,15 +99,36 @@ public class TestRE2Plugin {
         }
     }
 
+    static System.Threading.ReaderWriterLockSlim rwl = new();
+    static System.Collections.Concurrent.ConcurrentDictionary<long, bool> test = new(Environment.ProcessorCount * 2, 8192);
     public static void BenchFn() {
         var playerManager = API.GetManagedSingletonT<app.ropeway.PlayerManager>();
         var player = playerManager.get_CurrentPlayer();
         if (player != null) {
-            var playerController = player.getComponent(app.Collision.HitController.REFType.GetRuntimeType().As<_System.Type>());
-            if (playerController != null) {
-                for (int i = 0; i < 10000; ++i) {
-                    get_GameObjectFn.Invoke(playerController, null);
+            via.Component playerControllerRaw = player.getComponent(app.Collision.HitController.REFType.GetRuntimeType().As<_System.Type>());
+            if (playerControllerRaw != null) {
+                var playerController = (playerControllerRaw as IObject).As<app.Collision.HitController>();
+                for (int i = 0; i < 1; ++i) {
+                    //rwl.EnterReadLock();
+                    //rwl.ExitReadLock();
+                    //playerController.
+                    //playerController.get_DeltaTime();
+                    var gameobj = playerController.get_GameObject();
+
+                    if (gameobj != null) {
+                        /*var backToPlayerController = gameobj.getComponent(app.Collision.HitController.REFType.GetRuntimeType().As<_System.Type>());
+
+                        if (backToPlayerController != null) {
+                            //System.Console.WriteLine("Back to player controller!!!");
+                        }*/
+                    }
+                    //get_GameObjectFn.Invoke(playerController, null);
+                    //object result = null;
+                    //get_GameObjectFn.HandleInvokeMember_Internal(playerController, null, ref result);
                 }
+
+                var refCount = ((playerController.get_GameObject() as IProxy).GetInstance() as ManagedObject).GetReferenceCount();
+                System.Console.WriteLine("PlayerController ref count: " + refCount);
             }
         }
     }
@@ -103,7 +153,27 @@ public class RE2HookBenchmark {
     
     static System.Threading.ReaderWriterLockSlim rwl = new();
 
+    internal class ThreadData {
+        internal long threadID;
+        internal double totalMicros;
+        internal int callCount;
+        internal double highestMicros;
+        internal double runningAvg;
+        internal float[] benchmarkData = new float[1000];
+    };
+
+    internal static System.Collections.Concurrent.ConcurrentDictionary<long, ThreadData> threadData = new(Environment.ProcessorCount * 2, 8192);
+
     public static void Bench(System.Action action) {
+        var threadID = System.Threading.Thread.CurrentThread.ManagedThreadId;
+        ThreadData? data = null;
+        if (!threadData.ContainsKey(threadID)) {
+            data = new ThreadData() { threadID = threadID };
+            threadData.TryAdd(threadID, data);
+        } else {
+            threadData.TryGetValue(threadID, out data);
+        }
+
         var sw = _System.Diagnostics.Stopwatch.REFType.CreateInstance(0).As<_System.Diagnostics.Stopwatch>();
         //var sw2 = new System.Diagnostics.Stopwatch();
         sw.Start();
@@ -112,14 +182,28 @@ public class RE2HookBenchmark {
 
         sw.Stop();
         var elapsedTicks = (double)sw.get_ElapsedTicks();
+        var elapsedMicros = elapsedTicks / (double)TimeSpan.TicksPerMicrosecond;
+
+        data.totalMicros += elapsedMicros;
+        data.callCount++;
+
+        if (elapsedMicros > data.highestMicros) {
+            data.highestMicros = elapsedMicros;
+        }
+        
+        data.runningAvg = data.totalMicros / (double)data.callCount;
+        data.benchmarkData[data.callCount % 1000] = (float)elapsedMicros;
+
+        if (data.callCount >= 1000) {
+            data.callCount = 0;
+            data.totalMicros = 0.0;
+        }
 
         rwl.EnterWriteLock();
 
         callCount++;
 
         if (callCount >= 5) {
-            var elapsedMicros = elapsedTicks / (double)TimeSpan.TicksPerMicrosecond;
-
             totalMicros += elapsedMicros;
             MeasureCount++;
             RunningAvg = totalMicros / MeasureCount;
@@ -141,9 +225,14 @@ public class RE2HookBenchmark {
     static PreHookResult Pre(System.Span<ulong> args) {
         var hitController = ManagedObject.ToManagedObject(args[1]).As<app.Collision.HitController>();
 
-        /*Bench(() => {
-            hitController.get_GameObject();
-        });*/
+        //Bench(TestRE2Plugin.BenchFnAction);
+        Bench(() => {
+            for (int i = 0; i < 10000; ++i) {
+                var gameobj = hitController.get_GameObject();
+                if (gameobj != null) {
+                }
+            }
+        });
 
         return PreHookResult.Continue;
     }
