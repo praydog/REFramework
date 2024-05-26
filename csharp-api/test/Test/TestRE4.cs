b9651a1d0b7c7cd25683ce818599287e111b6475
praydog <praydog@praydog.com>
2024-05-25 22:40:59 -0700

    .NET: Update RE4 script

--- csharp-api/test/Test/TestRE4.cs
+++ csharp-api/test/Test/TestRE4.cs
@@ -5,12 +5,16 @@ using REFrameworkNET;
 using REFrameworkNET.Callbacks;
 using REFrameworkNET.Attributes;
 using _;
+using chainsaw;
 
 public class TestRE4Plugin {
     static bool IsRunningRE2 => Environment.ProcessPath.Contains("re4", StringComparison.CurrentCultureIgnoreCase);
+    static System.Diagnostics.Stopwatch imguiStopwatch = new();
     
     [Callback(typeof(ImGuiRender), CallbackType.Pre)]
     public static void ImGuiCallback() {
+        imguiStopwatch.Restart();
+
         if (ImGui.Begin("Test Window")) {
             ImGui.Text("RE4");
             ImGui.Separator();
@@ -22,11 +26,48 @@ public class TestRE4Plugin {
                 if (context == null) {
                     ImGui.Text("Player context is null");
                 } else {
-                    ImGui.Text($"Player context: {(context as IObject).Call("ToString", null) as string}");
+                    ImGui.Text($"Player context: {context}");
+
+                    var body = context.BodyGameObject;
+                    var head = context.HeadGameObject;
+
+                    bool drawBody = body.DrawSelf;
+                    if (ImGui.Checkbox("Draw body", ref drawBody)) {
+                        body.DrawSelf = drawBody;
+                    }
+
+                    ImGui.Text($"Body: {body}");
+                    ImGui.Text($"Head: {head}");
+
+                    if (ImGui.TreeNode("Components (Body)")) {
+                        var components = body.Components;
+
+                        for (int i = 0; i < components.Count; i++) {
+                            var component = components[i];
+                            ImGui.Text($"Component {i}: {component}");
+                        }
+
+                        ImGui.TreePop();
+                    }
+
+                    if (ImGui.TreeNode("Components (Head)")) {
+                        var components = head.Components;
+
+                        for (int i = 0; i < components.Count; i++) {
+                            var component = components[i];
+                            ImGui.Text($"Component {i}: {component}");
+                        }
+
+                        ImGui.TreePop();
+                    }
                 }
 
                 ImGui.TreePop();
             }
+
+            imguiStopwatch.Stop();
+            var microseconds = imguiStopwatch.ElapsedTicks / (System.Diagnostics.Stopwatch.Frequency / 1000000);
+            ImGui.Text($"Test execution time: {microseconds} µs");
 
             ImGui.End();
         }
