f8291a4ab3e6e5e1a86cdb45b4f604e921267b4e
praydog <praydog@praydog.com>
2026-03-10 14:14:19 -0700

    SDK: Migration of rendering structures from upscaler branch

--- shared/sdk/Memory.hpp
+++ shared/sdk/Memory.hpp
@@ -2,9 +2,13 @@
 
 namespace sdk {
 namespace memory {
-void* allocate(size_t size);
+void* allocate(size_t size, bool zero_memory = true);
 void deallocate(void* ptr);
 
 void* reallocate(void* ptr, size_t old_size, size_t size);
+
+namespace detail {
+void* allocate_plugin_loader(size_t size);
+}
 }
 }
