c58a40298206a2578ad55f3e91544075754305b2
hcdd0304 <hcdd0304@proton.me>
2026-08-11 22:24:52 +0700

    Add C# stop callbacks

--- csharp-api/REFrameworkNET/Callbacks.hpp
+++ csharp-api/REFrameworkNET/Callbacks.hpp
@@ -39,6 +39,11 @@ interface class ICallback {
 
 };
 
+public enum class CallbackAction {
+    Continue = 0,
+    Stop = 1
+};
+
 #define GENERATE_POCKET_CLASS(EVENT_NAME) \
 public ref class EVENT_NAME : public ICallback { \
 public: \
@@ -70,17 +75,45 @@ public: \
             PostImplementation(); \
         } \
     } \
+    /* Call this inside your callback to unsubscribe it after the current invocation. */ \
+    static void RequestStop() { \
+        s_wantsRemoval = true; \
+    } \
 internal: \
     static void TriggerPre() { \
-        Pre(); \
+        if (Callbacks::Impl::IsUnloading || PreImplementation == nullptr) { \
+            return; \
+        } \
+        auto delegates = PreImplementation->GetInvocationList(); \
+        for each (auto del in delegates) { \
+            auto typedDel = (BaseCallback::Delegate^)del; \
+            s_wantsRemoval = false; \
+            typedDel(); \
+            if (s_wantsRemoval) { \
+                PreImplementation -= typedDel; \
+            } \
+        } \
     } \
     static void TriggerPost() { \
-        Post(); \
+        if (Callbacks::Impl::IsUnloading || PostImplementation == nullptr) { \
+            return; \
+        } \
+        auto delegates = PostImplementation->GetInvocationList(); \
+        for each (auto del in delegates) { \
+            auto typedDel = (BaseCallback::Delegate^)del; \
+            s_wantsRemoval = false; \
+            typedDel(); \
+            if (s_wantsRemoval) { \
+                PostImplementation -= typedDel; \
+            } \
+        } \
     } \
     static BaseCallback::Delegate^ TriggerPreDelegate = gcnew BaseCallback::Delegate(&EVENT_NAME::TriggerPre); \
     static BaseCallback::Delegate^ TriggerPostDelegate = gcnew BaseCallback::Delegate(&EVENT_NAME::TriggerPost); \
     static BaseCallback::Delegate^ PreImplementation; \
     static BaseCallback::Delegate^ PostImplementation; \
+    [System::ThreadStatic] \
+    static bool s_wantsRemoval; \
 }; \
 
 namespace Callbacks {
